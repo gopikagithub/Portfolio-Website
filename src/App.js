a68b452874092f75e09fdbd610ee4943850d5a2e
Gopika Deepak <gopikadeepak45@gmail.com>
2023-03-14 20:46:08 +0400

    For Deploying

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Routes,Route } from 'react-router-dom';
+import './App.scss';
+import AboutMe from './routes/about-me/about-me.components';
+import Contact from './routes/contact/contact.component';
+import Education from './routes/education/education.components';
+import Home from './routes/home/home.component';
+import MyPortfolio from './routes/my-portfolio/my-portfolio.component';
+import Navigation from './routes/navigation/navigation.component';
+
+const App=()=>{
+  return(
+    <div className='app-container'>
+    <Routes>
+
+      <Route path='/Portfolio/' element={<Navigation/>}>
+
+        <Route index element={<Home/>}/>
+        <Route path='aboutMe' element={<AboutMe/>}/>
+        <Route path='education' element={<Education/>}/>
+        <Route path='myPortfolio' element={<MyPortfolio/>}/>
+        <Route path='contact' element={<Contact/>}/>
+
+      </Route>
+
+    </Routes>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/routes/about-me/about-me.components.jsx
@@ -0,0 +1,48 @@
+import './about-me.styles.scss'
+
+const AboutMe=()=>{
+    return(
+        <div className="about-me-container">
+            <h1>   <span> About </span>Me</h1><br/>
+            <hr/>
+
+            <div className="about-data">
+
+                <div className="personal-data">
+                    <h3>Personal Data</h3>
+                    <ul>
+                        <li> Name  :  Gopika Deepak</li>
+                        <li>Age : 29</li>
+                        <li>Qualification : B-Tech CSE</li>
+                        <li>Language : English,Malayalam</li>
+                    </ul>
+                                        
+                </div>
+
+                <div className=" tech-skills"> 
+                        <h3>Technical skills</h3>
+                        <ul>
+                            <li>C,C++,Java,Java Script</li> 
+                            <li>HTML/HTML5,CSS/CSS3</li>
+                            <li>Node.js</li>
+                            <li>React</li>
+                            <li>React Native</li>
+                            <li>Express.js</li>
+                            <li>MongoDB,SQL,Oracle,MySql</li>
+                            <li>Windows XP/7/10</li>
+                            <li>Firebase</li>
+                            <li>Git,GitHub</li>
+                            <li>Redux</li>
+                            <li>SASS</li>
+                        </ul>
+                </div>
+
+                 
+            
+            </div>
+           
+        </div>
+    )
+}
+
+export default AboutMe;
